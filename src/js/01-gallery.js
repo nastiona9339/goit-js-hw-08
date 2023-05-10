57c10a0eaf64db4e71a5b2c95eceb8b3a0207a32
Anastasiia Derdiai <115905772+nastiona9339@users.noreply.github.com>
2023-05-10 12:17:13 -0500

    first commit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,41 @@
 // Add imports above this line
+
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log("galleryItems");
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+
+const callToGallary = document.querySelector(".gallery")
+
+function creatItem() {
+    let  elements = ""
+        
+    galleryItems.forEach(item => 
+        elements += `<li class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      alt="${item.description}"
+      title="${item.description}"
+    />
+  </a>
+</li>`)
+    
+    callToGallary.innerHTML = elements
+}
+
+creatItem()
+
+
+let lightbox = new SimpleLightbox('.gallery a', { captionDelay : 250 });
+lightbox.on('show.simplelightbox', function () {
+});
+
+console.log(galleryItems);
+
+console.log(galleryItems);
