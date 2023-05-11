6e051cf052bf360f3fbf26cb3c26a0bf73a63520
Anastasiia Derdiai <115905772+nastiona9339@users.noreply.github.com>
2023-05-10 23:39:20 -0500

    third_commit

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+const video = document.querySelector("#vimeo-player")
 
+
+const player = new Player(video);
+
+const onPlay = function({
+    seconds: 3.03}) {
+   
+};
+
+player.on('timeupdate', onPlay);
+
+
+
+console.log(onPlay);
