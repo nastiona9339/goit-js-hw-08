0ba9b42d6ad8e231cb5164ed7c85bfdf58a25498
Anastasiia Derdiai <115905772+nastiona9339@users.noreply.github.com>
2023-05-11 13:13:43 -0500

    last_commit

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,16 +1,47 @@
 import Player from '@vimeo/player';
 const video = document.querySelector("#vimeo-player")
-
+const throttle = require('lodash.throttle');
 
 const player = new Player(video);
+ 
 
-const onPlay = function({
-    seconds: 3.03}) {
+ function pushInfo (time) {
+   localStorage.setItem("videoplayer-current-time", JSON.stringify(time) ) 
+} 
+
+function pullInfo() {
+ return JSON.parse(localStorage.getItem("videoplayer-current-time"))   
+}
+
+
+
+player.on("timeupdate", throttle (onUpDate, 1000))
+
+function onUpDate() {
+    const timeUpDate = player.getCurrentTime().then(function (seconds) {
+     pushInfo(seconds);
+    
+}).catch(function(error) {
    
-};
-
-player.on('timeupdate', onPlay);
-
+});
+}
 
 
-console.log(onPlay);
+
+
+player.on('play', onPlay) 
+function onPlay() {
+    let currentTime = pullInfo()
+
+    player.setCurrentTime(currentTime).then(function(seconds) {
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            break;
+
+        default:
+            break;
+    }
+});
+}
+
