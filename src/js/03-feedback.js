6e3b2172772eed4b97d9215850532717262a6d78
Anastasiia Derdiai <115905772+nastiona9339@users.noreply.github.com>
2023-05-11 15:40:35 -0500

    last_commit

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,55 @@
+const throttle = require('lodash.throttle');
 
+const form = document.querySelector(".feedback-form")
+
+form.addEventListener("input", throttle(onInput, 500))
+
+
+
+
+let info = pullInfo()
+
+
+if (info) {
+    form.elements.email.value = info.email
+    form.elements.message.value = info.message
+} else {
+    info = {
+        email: "",
+        message: "",
+    }
+}
+
+
+function  onInput(e) {
+    if (e.target.name === "email")  {
+    info.email = e.target.value.trim()
+    }
+    
+    if (e.target.name === "message") {
+    info.message = e.target.value.trim()
+    }
+ 
+    pushInfo(info)
+}
+    
+
+ 
+
+
+ function pushInfo (obj) {
+   localStorage.setItem("feedback-form-state", JSON.stringify(obj) ) 
+} 
+
+function pullInfo() {
+ return JSON.parse(localStorage.getItem("feedback-form-state"))   
+}
+
+form.addEventListener("submit", onSubmit) 
+
+function onSubmit(e) {
+    e.preventDefault()
+    localStorage.clear()
+    console.log(info);
+    form.reset()
+    }
